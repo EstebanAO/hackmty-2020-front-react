db180ba2e2dd9adef916fd772aa6d338f439615b
Eduardo Trujillo <edytr1997@gmail.com>
2020-08-29 23:33:43 -0500

    Implement the logic for question

--- src/components/tests/question.js
+++ src/components/tests/question.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Typography, Row, Col, Space } from 'antd'
 import axios from 'axios'
 
+import { API_URL } from '../../constants/routes'
+
 const { Title } = Typography
 
 const questionStyle = {
@@ -49,47 +51,104 @@ export class Question extends Component {
   constructor() {
     super()
     this.state = {
+      countdown: 10,
+      started_at_in_epoch: 0,
+      query: "Como me llamo?",
+      option_a: "",
+      option_b: "",
+      option_c: "",
+      option_d: "",
+      correct_option: "",
+      is_correct: [true, true, true, true],
     }
+  }
+
+  componentDidMount() {
+    axios.get(API_URL + '/question').then(res => {
+      res['countdown'] = 30
+      res['is_correct'] = [true, true, true, true]
+      this.setState(res)
+      this.interval = setInterval(() => {
+        if (this.state.countdown !== 0) {
+          this.setState({['countdown']: this.state.countdown - 1})
+        } else {
+          this.showCorrectAnswer()
+        }
+      }, 1000)
+    }).catch(err =>
+      console.log(err)
+    )
+  }
+
+  showCorrectAnswer() {
+    clearInterval(this.interval)
+    const is_correct = []
+    for (let i = 1; i <= 4; i++) {
+      if (i === Number(this.state.correct_option)) {
+        is_correct.push(true)
+      } else {
+        is_correct.push(false)
+      }
+    }
+    this.setState({['is_correct']: is_correct})
   }
 
   render() {
     return (
       <>
         <Row style={questionStyle}>
-          <Col offset={6} span={12}>
-            <Title style={{margin: '30px'}}>1. Cuando se fundo Microsoft?</Title>
+          <Col offset={5} span={14}>
+            <Title style={{textAlign: 'center', margin: '30px'}}>{this.state.query}</Title>
           </Col>
         </Row>
         <Row>
-          <Col offset={11} span={2}>
-            <Title style={{marginTop: '30px', fontSize: '120px'}}>30</Title>
+          <Col offset={9} span={6}>
+            <Title style={{textAlign: 'center', marginTop: '30px', fontSize: '120px'}}>
+              {this.state.countdown}
+            </Title>
           </Col>
         </Row>
         <Row>
-          <Col offset={2} span={9} style={{...optionStyle, ...optionAStyle}}>
+          <Col
+            offset={2}
+            span={9}
+            style={{...optionStyle, ...optionAStyle, opacity: this.state.is_correct[0] ? 1 : 0.4}}
+          >
             <Space direction="vertical">
               <Title level={1} style={optionTextStyle}>1</Title>
-              <Title level={2} style={optionTextStyle}>Respuesta numero 1</Title>
+              <Title level={2} style={optionTextStyle}>{this.state.option_a}</Title>
             </Space>
           </Col>
-          <Col offset={2} span={9} style={{...optionStyle, ...optionBStyle}}>
+          <Col
+            offset={2}
+            span={9}
+            style={{...optionStyle, ...optionBStyle, opacity: this.state.is_correct[1] ? 1 : 0.4}}
+          >
             <Space direction="vertical">
               <Title level={1} style={optionTextStyle}>2</Title>
-              <Title level={2} style={optionTextStyle}>Respuesta numero 1</Title>
+              <Title level={2} style={optionTextStyle}>{this.state.option_b}</Title>
             </Space>
           </Col>
         </Row>
         <Row>
-          <Col offset={2} span={9} style={{...optionStyle, ...optionCStyle}}>
+          <Col
+            offset={2}
+            span={9}
+            style={{...optionStyle, ...optionCStyle, opacity: this.state.is_correct[2] ? 1 : 0.4}}
+          >
             <Space direction="vertical">
               <Title level={1} style={optionTextStyle}>3</Title>
-              <Title level={2} style={optionTextStyle}>Respuesta numero 1</Title>
+              <Title level={2} style={optionTextStyle}>{this.state.option_c}</Title>
             </Space>
           </Col>
-          <Col offset={2} span={9} style={{...optionStyle, ...optionDStyle}}>
+          <Col
+            offset={2}
+            span={9}
+            style={{...optionStyle, ...optionDStyle, opacity: this.state.is_correct[3] ? 1 : 0.4}}
+          >
             <Space direction="vertical">
               <Title level={1} style={optionTextStyle}>4</Title>
-              <Title level={2} style={optionTextStyle}>Respuesta numero 1</Title>
+              <Title level={2} style={optionTextStyle}>{this.state.option_d}</Title>
             </Space>
           </Col>
         </Row>
