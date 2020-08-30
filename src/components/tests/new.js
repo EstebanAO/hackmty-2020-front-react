43a0d5435d4f512ae9d516f9a656c3b026269090
Rene Garcia <rene.saaenz@gmail.com>
2020-08-30 09:21:21 -0500

    Redirects after quiz creation

--- src/components/tests/new.js
+++ src/components/tests/new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Divider, Form, Input, Row, Col, Space, Button, Radio } from 'antd';
+import { Redirect } from 'react-router-dom'
 import axios from 'axios'
 import * as ROUTES from '../../constants/routes';
 
@@ -9,6 +10,7 @@ export class Index extends Component {
     this.state = {
       name: "",
       questions: [],
+      test_submitted: false,
     }
     this.handleChange = this.handleChange.bind(this)
     this.appendQuestion = this.appendQuestion.bind(this)
@@ -24,7 +26,12 @@ export class Index extends Component {
     const body = JSON.stringify(data)
     console.log(body)
     axios.post(ROUTES.API_URL+ "/quizzes", data, { mode: 'no-cors' }
-    ).then(res => console.log(res)
+    ).then(res => {
+      console.log(res)
+      this.setState({
+        test_submitted: true,
+      })
+    }
     ).catch(err =>
       console.log(err)
     )
@@ -75,135 +82,138 @@ export class Index extends Component {
   }
 
   render() {
+    if (this.state.test_submitted) {
+      return (<Redirect to={"/tests"} />)
+    }
     const questions = this.state.questions.map((question, i) => {
       return (
         <>
-          <Divider></Divider>
-          <Row>
-            <Col span={2} offset={5}>
-              Pregunta {i + 1}
-            </Col>
-            <Col span={13}>
-              <Input.TextArea
-                size="medium"
-                placeholder="Escribe una pregunta"
-                name="query"
-                value={this.state.questions[i].query}
-                onChange={(event) => this.handleQuestion(event, i)}
-              />
-            </Col>
-          </Row>
-          <Row style={{marginTop: '15px'}}>
-            <Col span={2} offset={5}>
-              Selecciona la respuesta
-            </Col>
-            <Col>
-              <Radio.Group
-                name="correct_option"
-                value={this.state.questions[i].correct_option}
-                onChange={(event) => this.handleQuestion(event, i)}
-              >
-                <Radio.Button value="1">1</Radio.Button>
-                <Radio.Button value="2">2</Radio.Button>
-                <Radio.Button value="3">3</Radio.Button>
-                <Radio.Button value="4">4</Radio.Button>
-              </Radio.Group>
-            </Col>
-          </Row>
-          <Row style={{marginTop: '15px'}}>
-            <Col span={2} offset={5}>
-              Opción 1
-            </Col>
-            <Col span={5}>
-              <Input
-                size="medium"
-                placeholder="Escribe una respuesta"
-                name="option_a"
-                value={this.state.questions[i].option_a}
-                onChange={(event) => this.handleQuestion(event, i)}
-              />
-            </Col>
-            <Col span={2} offset={1}>
-              Opción 2
-            </Col>
-            <Col span={5}>
-              <Input
-                size="medium"
-                placeholder="Escribe una respuesta"
-                name="option_b"
-                value={this.state.questions[i].option_b}
-                onChange={(event) => this.handleQuestion(event, i)}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col span={2} offset={5}>
-              Opción 3
-            </Col>
-            <Col span={5}>
-              <Input
-                size="medium"
-                placeholder="Escribe una respuesta"
-                name="option_c"
-                value={this.state.questions[i].option_c}
-                onChange={(event) => this.handleQuestion(event, i)}
-              />
-            </Col>
-            <Col span={2} offset={1}>
-              Opción 4
-            </Col>
-            <Col span={5}>
-              <Input
-                size="medium"
-                placeholder="Escribe una respuesta"
-                name="option_d"
-                value={this.state.questions[i].option_d}
-                onChange={(event) => this.handleQuestion(event, i)}
-              />
-            </Col>
-          </Row>
+        <Divider></Divider>
+        <Row>
+        <Col span={2} offset={5}>
+        Pregunta {i + 1}
+        </Col>
+        <Col span={13}>
+        <Input.TextArea
+        size="medium"
+        placeholder="Escribe una pregunta"
+        name="query"
+        value={this.state.questions[i].query}
+        onChange={(event) => this.handleQuestion(event, i)}
+        />
+        </Col>
+        </Row>
+        <Row style={{marginTop: '15px'}}>
+        <Col span={2} offset={5}>
+        Selecciona la respuesta
+        </Col>
+        <Col>
+        <Radio.Group
+        name="correct_option"
+        value={this.state.questions[i].correct_option}
+        onChange={(event) => this.handleQuestion(event, i)}
+        >
+        <Radio.Button value="1">1</Radio.Button>
+        <Radio.Button value="2">2</Radio.Button>
+        <Radio.Button value="3">3</Radio.Button>
+        <Radio.Button value="4">4</Radio.Button>
+        </Radio.Group>
+        </Col>
+        </Row>
+        <Row style={{marginTop: '15px'}}>
+        <Col span={2} offset={5}>
+        Opción 1
+        </Col>
+        <Col span={5}>
+        <Input
+        size="medium"
+        placeholder="Escribe una respuesta"
+        name="option_a"
+        value={this.state.questions[i].option_a}
+        onChange={(event) => this.handleQuestion(event, i)}
+        />
+        </Col>
+        <Col span={2} offset={1}>
+        Opción 2
+        </Col>
+        <Col span={5}>
+        <Input
+        size="medium"
+        placeholder="Escribe una respuesta"
+        name="option_b"
+        value={this.state.questions[i].option_b}
+        onChange={(event) => this.handleQuestion(event, i)}
+        />
+        </Col>
+        </Row>
+        <Row>
+        <Col span={2} offset={5}>
+        Opción 3
+        </Col>
+        <Col span={5}>
+        <Input
+        size="medium"
+        placeholder="Escribe una respuesta"
+        name="option_c"
+        value={this.state.questions[i].option_c}
+        onChange={(event) => this.handleQuestion(event, i)}
+        />
+        </Col>
+        <Col span={2} offset={1}>
+        Opción 4
+        </Col>
+        <Col span={5}>
+        <Input
+        size="medium"
+        placeholder="Escribe una respuesta"
+        name="option_d"
+        value={this.state.questions[i].option_d}
+        onChange={(event) => this.handleQuestion(event, i)}
+        />
+        </Col>
+        </Row>
         </>
       )
     })
     return (
       <>
-        <Row style={{margin: '15px'}}>
-          <h1>
-            Crear examen
-          </h1>
-        </Row>
-        <Row>
-          <Col offset={6} span={12}>
-            <Input
-              size="large"
-              placeholder="Nombre del examen"
-              name="name"
-              value={this.state.name}
-              style={{ marginTop: '10px' }}
-              onChange={this.handleChange}
-            />
-          </Col>
-        </Row>
-        <Row style={{marginTop: '15px'}}>
-          <Col span={6} offset={11}>
-            <Button
-              onClick={this.appendQuestion}
-            >
-              Agregar pregunta
-            </Button>
-          </Col>
-        </Row>
-        {questions}
-        <Row style={{marginTop: '30px', marginBottom: '15px'}}>
-          <Col span={6} offset={11}>
-            <Button
-              type="primary"
-              onClick={this.uploadQuiz}
-            >
-              Subir cuestionario
-            </Button>
-          </Col>
-        </Row>
+      <Row style={{margin: '15px'}}>
+      <h1>
+      Crear examen
+      </h1>
+      </Row>
+      <Row>
+      <Col offset={6} span={12}>
+      <Input
+      size="large"
+      placeholder="Nombre del examen"
+      name="name"
+      value={this.state.name}
+      style={{ marginTop: '10px' }}
+      onChange={this.handleChange}
+      />
+      </Col>
+      </Row>
+      <Row style={{marginTop: '15px'}}>
+      <Col span={6} offset={11}>
+      <Button
+      onClick={this.appendQuestion}
+      >
+      Agregar pregunta
+      </Button>
+      </Col>
+      </Row>
+      {questions}
+      <Row style={{marginTop: '30px', marginBottom: '15px'}}>
+      <Col span={6} offset={11}>
+      <Button
+      type="primary"
+      onClick={this.uploadQuiz}
+      >
+      Subir cuestionario
+      </Button>
+      </Col>
+      </Row>
       </>
     )
   }
